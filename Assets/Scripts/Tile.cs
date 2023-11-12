27b04b14726be123138db369006e06ef7a8d2de8
Adam Graham <adam@zigurous.com>
2023-11-12 16:32:04 -0600

    Add projects assets

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,41 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tile : MonoBehaviour
+{
+    [System.Serializable]
+    public class State
+    {
+        public Color fillColor;
+        public Color outlineColor;
+    }
+
+    public State state { get; private set; }
+    public char letter { get; private set; }
+
+    private Image fill;
+    private Outline outline;
+    private TextMeshProUGUI text;
+
+    private void Awake()
+    {
+        fill = GetComponent<Image>();
+        outline = GetComponent<Outline>();
+        text = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    public void SetLetter(char letter)
+    {
+        this.letter = letter;
+        text.text = letter.ToString();
+    }
+
+    public void SetState(State state)
+    {
+        this.state = state;
+        fill.color = state.fillColor;
+        outline.effectColor = state.outlineColor;
+    }
+
+}
